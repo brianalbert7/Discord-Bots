153aea8e9015b1b978f3177a6dfb76f8f3e170a3
Brian Albert <57815410+brianalbert7@users.noreply.github.com>
2023-01-13 21:49:33 -0500

    added muah bot

---
+++ discord_sheck.js
@@ -0,0 +1,18 @@
+// Must have npm installed and run 'npm install discord.js' in this directory to download the discord API
+
+const Discord = require('discord.js');
+const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.MessageContent,]});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content.toLowerCase().includes('muah')) {
+    console.log('muah')
+    message.channel.send({ files: ['./muah.jpg'] });
+  }
+});
+
+client.login('MTA2MjUzNjM1MzY0NjEyOTE4Mg.GHBX73.yc7yFKGHIZ15TDmE083sWkVwqrZUlq1A64g_ds');
